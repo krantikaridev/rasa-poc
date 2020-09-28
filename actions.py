c340a139e06dc4d69f725b2653e4197ddc6d7780
madanmeena <madan.mohan@quovantis.com>
2020-09-28 14:38:20 +0530

    different fallback based on random number

--- actions.py
+++ actions.py
@@ -6,29 +6,42 @@
 
 
 # This is a simple example for a custom action which utters "Hello World!"
-
-from typing import Any, Text, Dict, List
-
+from typing import Any, Text, Dict, List, Optional, Union
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import (SlotSet, SessionStarted, ActionExecuted, EventType, ConversationPaused, UserUtteranceReverted)
+import logging
+logger = logging.getLogger(__name__)
+from random import seed
+from random import randint
 
 class ActionFallback(Action):
 
     def name(self) -> Text:
         return "action_default_fallback"
 
-    # def __init__(self) -> None:
-    #     super(ActionFallback, self).__init__("utter_default")
+    # def __init__(self):
+    #     super(ActionFallback, self).__init__("utter_default", silent_fail=True)
 
-    async def run(
+    # https://github.com/RasaHQ/rasa-demo/blob/master/actions/actions.py
+    def run(
         self,
-        output_channel: "OutputChannel",
-        nlg: "NaturalLanguageGenerator",
-        tracker: "DialogueStateTracker",
-        domain: "Domain",
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
     ) -> List[EventType]:
-        # only utter the response if it is available
-        evts = await super(ActionFallback, self).run(output_channel, nlg, tracker, domain)
+        
+        # only utter the template if it is available
+        seed(1)
+        number_of_fallback= randint(0, 100)
+        logger.debug("Hit fall back.")
+        logger.info(f"number_of_fallback:{number_of_fallback}")
+        if number_of_fallback%2==1:
 
-        return evts + [UserUtteranceReverted()]
+            dispatcher.utter_message(template="utter_iamabot")
+
+        # Fallback caused by Core
+        else:
+            dispatcher.utter_message(template="utter_default")
+        
+        return [UserUtteranceReverted()]
